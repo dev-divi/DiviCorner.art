f4000caedb29a5f3bbad7f534730eeb58520c7c5
James T. Bytes <91709492+dev-divi@users.noreply.github.com>
2023-01-03 13:44:06 -0800

    Fire

--- pages/MainAbout/AboutInfo.js
+++ pages/MainAbout/AboutInfo.js
@@ -25,16 +25,18 @@ const AboutInfo = () => {
                 There are people who believe that much of this knowledge should not be available to the public, 
                 due to how civilian populations could misuse power, 
                 and this is why it remains hidden. 
- */}
-                <br /> <br /> 
+                
                 Refinement Process: <br / >
                 All information should point toward human freedom. 
                 {/* This app has been designed to store only the core root of a secret&apos;s information, explaining a critically important concept in few words. <br /> */}
-                All texts should be entry level. <br /> 
+                {/* All texts should be entry level. <br /> 
                 All texts should be non-biased. <br /> 
                 Redundancy should be removed. <br /> 
                 No text should be exclusive to a group. <br /> 
                 
+ */}
+            What might be possible? 
+                <br /> <br /> 
  
              </p>
                 
@@ -46,6 +48,7 @@ const AboutInfo = () => {
             
                 unlike abstract philosophy that can be difficult or confusing to adapt to the world you live in. 
             */ }
+
             <p> <br /> 
             Join the email list to be notified of new works. </p>
             
@@ -57,7 +60,7 @@ const AboutInfo = () => {
 <br /> 
                 <br/> 
                 bitcoin donation address: bc1q99vng5nykfjv7vy8crj7p3a4nluwgm44mup20a<br /> 
-ethereum donation address: 0x39c7C9FdB10Df996d59590fd5cC52483a919EB29  <br /> 
+                ethereum donation address: 0x39c7C9FdB10Df996d59590fd5cC52483a919EB29  <br /> 
 <br /> <br /> 
             Thank you for being here. 
 
